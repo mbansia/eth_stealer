72ec371844c77845457c0504517ce6a89a13b767
mbansia <31772936+mbansia@users.noreply.github.com>
2018-04-17 17:01:45 +0530

    Update eth_stealer.py

--- eth_stealer.py
+++ eth_stealer.py
@@ -1,3 +1,5 @@
+
+
 from ecdsa import SigningKey, SECP256k1
 import sha3
 from ethplorer.address import Address
@@ -5,14 +7,17 @@ from ethplorer.address import Address
 i=0
 
 while True:
+    #Code taken from Vincent Kobel. License at: https://github.com/vkobel/ethereum-generate-wallet/blob/master/LICENSE.md
     keccak = sha3.keccak_256()
     
     priv = SigningKey.generate(curve=SECP256k1)
-
+    
     pub = priv.get_verifying_key().to_string()
+        
     keccak.update(pub)
     address = keccak.hexdigest()[24:]
     address = '0x' + address
+    #End of attributed code
     
     ADD = Address(address = address)
     
@@ -27,3 +32,31 @@ while True:
     if (i % 100) == 0:
         print(i)
     i=i+1
+
+    
+"""MIT License
+Copyright (c) 2018 Vincent Kobel
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+"""
+MIT License
+Copyright (c) 2018 Milind Bansia
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
